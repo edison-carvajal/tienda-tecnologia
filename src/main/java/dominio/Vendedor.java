9e0ffcaf1aebdc7c818322d3d07d80bd6610a8ba
Edison Carvajal <edisonacarvajal@gmail.com>
2019-06-14 13:30:22 -0500

    Solucion pruebas unitarias iniciales

--- src/main/java/dominio/Vendedor.java
+++ src/main/java/dominio/Vendedor.java
@@ -1,29 +1,52 @@
 package dominio;
 
 import dominio.repositorio.RepositorioProducto;
+import dominio.excepcion.GarantiaExtendidaException;
 import dominio.repositorio.RepositorioGarantiaExtendida;
 
 public class Vendedor {
 
-    public static final String EL_PRODUCTO_TIENE_GARANTIA = "El producto ya cuenta con una garantia extendida";
+	public static final String EL_PRODUCTO_TIENE_GARANTIA = "El producto ya cuenta con una garantia extendida";
 
-    private RepositorioProducto repositorioProducto;
-    private RepositorioGarantiaExtendida repositorioGarantia;
+	private static final String EL_PRODUCTO_NO_EXISTE = "El producto no existe";
 
-    public Vendedor(RepositorioProducto repositorioProducto, RepositorioGarantiaExtendida repositorioGarantia) {
-        this.repositorioProducto = repositorioProducto;
-        this.repositorioGarantia = repositorioGarantia;
+	private RepositorioProducto repositorioProducto;
+	private RepositorioGarantiaExtendida repositorioGarantia;
 
-    }
+	public Vendedor(RepositorioProducto repositorioProducto, RepositorioGarantiaExtendida repositorioGarantia) {
+		this.repositorioProducto = repositorioProducto;
+		this.repositorioGarantia = repositorioGarantia;
 
-    public void generarGarantia(String codigo) {
+	}
 
-        throw new UnsupportedOperationException("Método pendiente por implementar");
+	public void generarGarantia(String codigo) {
 
-    }
+		Producto producto;
 
-    public boolean tieneGarantia(String codigo) {
-        return false;
+		if (tieneGarantia(codigo)) {
+			throw new GarantiaExtendidaException(EL_PRODUCTO_TIENE_GARANTIA);
+		} else {
+
+			producto = repositorioProducto.obtenerPorCodigo(codigo);
+			if (producto == null) {
+				throw new GarantiaExtendidaException(EL_PRODUCTO_NO_EXISTE);
+			} else {
+				GarantiaExtendida garantia = new GarantiaExtendida(producto);
+				repositorioGarantia.agregar(garantia);
+			}
+		}
+
+	}
+
+	public boolean tieneGarantia(String codigo) {
+    	
+    	if(!codigo.isEmpty()){
+    		Producto producto = repositorioGarantia.obtenerProductoConGarantiaPorCodigo(codigo);
+    		if(producto != null){
+    			return true;
+    		}
+    	}
+    	return false;
     }
 
 }
